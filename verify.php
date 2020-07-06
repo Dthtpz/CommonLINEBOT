e85b28dcd4fda9d397ea420794168669906b54d7
Dthtpz <60869031+Dthtpz@users.noreply.github.com>
2020-07-06 23:48:13 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'LINE_token_ID';    //PUT LINE token ID at "Channel access token (long-lived)"
+$access_token = 'ddEV77o8kdwAMupkgssFq+szs6dwZH8qV7LmT41hFxkqXc83J+lJF/ZJVMxG502jUwLTn/JhObdBJNSLb2IOwNeT9K5syYYN/gmdqXPE1IbYpt5mAfjFieZ57deh+vqNc0Vu5OqrldfxRcNIiK5TZQdB04t89/1O/w1cDnyilFU=';    //PUT LINE token ID at "Channel access token (long-lived)"
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 
